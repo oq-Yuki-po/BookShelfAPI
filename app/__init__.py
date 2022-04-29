47be4898aae4a5d3c2b67398417cc76f82fedd24
oq-Yuki-po <o.yukinko0715@gmail.com>
2022-04-29 16:56:52 +0000

    test

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,1 @@
-import os
-import sys
-from pathlib import Path
 
-print("############")
-print(sys.path)
-sys.path.append(os.getcwd())
-sys.path.append(str(Path(os.getcwd()).parent))
-
-print("############")
-print(sys.path)
