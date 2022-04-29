203e6baa6a96976c599e0197e2a00a693e1de203
oq-Yuki-po <o.yukinko0715@gmail.com>
2022-04-29 16:52:42 +0000

    add env

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,11 @@
 import os
 import sys
+from pathlib import Path
 
+print("############")
+print(sys.path)
 sys.path.append(os.getcwd())
+sys.path.append(str(Path(os.getcwd()).parent))
+
+print("############")
+print(sys.path)
