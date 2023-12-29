1975cd6bbb607eb05331dbf5c10fb0878b857f64
oq-Yuki-po <o.yukinko0715@gmail.com>
2023-12-30 07:01:11 +0900

    Update access token field in UserLoginOut response schema

--- app/schemas/responses/user.py
+++ app/schemas/responses/user.py
@@ -15,13 +15,13 @@ class UserSaveOut(BaseModel):
 
 class UserLoginOut(BaseModel):
 
-    token: str = Field(title='token', min_length=1, max_length=255)
+    access_token: str = Field(title='token', min_length=1, max_length=255)
     type: str = Field(title='type', min_length=1, max_length=255, default='bearer')
 
     class ConfigDict:
         json_schema_extra = {
             'example': {
-                'token': 'token',
+                'access_token': 'token',
                 'type': 'bearer'
             }
         }
