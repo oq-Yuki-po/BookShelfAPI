288fcf9fac6fea86b8393f2e51e3f8cd11703d14
oq-Yuki-po <o.yukinko0715@gmail.com>
2024-01-07 02:28:59 +0900

    Add book saving functionality to book router

--- app/routers/book_router.py
+++ app/routers/book_router.py
@@ -1,8 +1,59 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
-from app.routers.setting import AppRoutes
+from app.exceptions.exceptions import NotEnoughPermissionsException
+from app.models import AuthorModel, BookAuthorModel, BookModel, session
+from app.routers.setting import AppRoutePermissions, AppRoutes
+from app.schemas.requests import BooksGoogleBooksApiSaveIn
+from app.schemas.responses import GoogleBooksApiSaveOut
+from app.services.google_books_api_service import GoogleBooksApiService
+from app.services.image_service import ImageBase64Service
+from app.services.login_service import LoginService, TokenData
 
 router = APIRouter(
     prefix=AppRoutes.Books.PREFIX,
     tags=[AppRoutes.Books.TAG]
 )
+BOOK_ROUTERS = AppRoutes.Books
+BOOK_ROUTER_PERMISSIONS = AppRoutePermissions.Books
+
+
+@router.post(BOOK_ROUTERS.POST_GOOGLE_BOOKS_URL,
+             response_model=GoogleBooksApiSaveOut,
+             status_code=200)
+async def save_google_books(books_google_books_api_save_in: BooksGoogleBooksApiSaveIn,
+                            current_user: TokenData = Depends(LoginService.verify_token)) -> GoogleBooksApiSaveOut:
+
+    # check user permissions
+    if current_user.role not in BOOK_ROUTER_PERMISSIONS.PostGoogleBooks.PERMISSIONS:
+        raise NotEnoughPermissionsException()
+
+    # fetch book data from google books api
+    book_data = GoogleBooksApiService.fetch_book_data(isbn=books_google_books_api_save_in.isbn)
+
+    # save author
+    for author in book_data.authors:
+        AuthorModel.save(name=author)
+
+    # save cover image
+    cover_image_path = GoogleBooksApiService.save_cover_image(google_book_schema=book_data,
+                                                              isbn=books_google_books_api_save_in.isbn)
+    # save book
+    book_model = BookModel(title=book_data.title,
+                           isbn=books_google_books_api_save_in.isbn,
+                           cover_path=cover_image_path,
+                           published_at=book_data.published_at)
+    new_book_model = book_model.save_google_books_api()
+
+    # save book author
+    author_models = AuthorModel.fetch_by_names(names=book_data.authors)
+
+    for author_model in author_models:
+        BookAuthorModel.save(book_id=new_book_model.id, author_id=author_model.id)
+
+    session.commit()
+
+    return GoogleBooksApiSaveOut(message='book saved successfully',
+                                 title=book_data.title,
+                                 authors=book_data.authors,
+                                 published_at=book_data.published_at,
+                                 cover_image_base64=ImageBase64Service.encode(image_path=cover_image_path))
---
+++ test/routers/test_book_router.py
@@ -0,0 +1,74 @@
+import datetime
+import os
+from typing import List
+
+from fastapi import status
+from fastapi.testclient import TestClient
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+from app.models import AuthorModel, BookAuthorModel, BookModel
+from app.routers.setting import AppRoutes
+from app.schemas.api import GoogleBookSchema
+from app.schemas.requests import BooksGoogleBooksApiSaveIn
+from app.schemas.responses import GoogleBooksApiSaveOut
+
+TEST_URL = f"{AppRoutes.Books.PREFIX}"
+
+
+def test_save_google_books_success(app_client: TestClient,
+                                   db_session: Session,
+                                   mocker,
+                                   override_verify_token_dependency,
+                                   encode_image_base64):
+    """
+    Test save google books
+    """
+    # Prepare
+    with override_verify_token_dependency("admin"):
+
+        test_isbn = "9784774193684"
+        books_google_books_api_save_in = BooksGoogleBooksApiSaveIn(isbn=test_isbn)
+        # Mock
+        mocker.patch('app.services.google_books_api_service.GoogleBooksApiService.fetch_book_data',
+                     return_value=GoogleBookSchema(title="test_title",
+                                                   authors=["test_author 1", "test_author 2"],
+                                                   published_at="2021-01-01",
+                                                   cover_url="https://via.placeholder.com/150"))
+        # Execute
+        response = app_client.post(f"{TEST_URL}{AppRoutes.Books.POST_GOOGLE_BOOKS_URL}",
+                                   json=books_google_books_api_save_in.model_dump())
+
+    # Assert
+    stmt = select(BookModel).where(BookModel.isbn == test_isbn)
+    book_model: BookModel = db_session.execute(stmt).scalars().first()
+    stmt = select(AuthorModel).where(AuthorModel.name.in_(["test_author 1", "test_author 2"]))
+    author_models: List[AuthorModel] = db_session.execute(stmt).scalars().all()
+    stmt = select(BookAuthorModel).where(BookAuthorModel.book_id == book_model.id)
+    book_author_models: List[BookAuthorModel] = db_session.execute(stmt).scalars().all()
+
+    # Check response
+    assert response.status_code == status.HTTP_200_OK
+    encoded_cover_image = encode_image_base64(f"app/static/images/{test_isbn}.jpg")
+    assert response.json() == GoogleBooksApiSaveOut(message='book saved successfully',
+                                                    title="test_title",
+                                                    authors=["test_author 1", "test_author 2"],
+                                                    published_at="2021-01-01",
+                                                    cover_image_base64=encoded_cover_image).model_dump()
+    # Check database
+    assert book_model.title == "test_title"
+    assert book_model.isbn == test_isbn
+    assert book_model.published_at == datetime.date(2021, 1, 1)
+    assert book_model.cover_path == f"app/static/images/{test_isbn}.jpg"
+    assert len(author_models) == 2
+    for author_model in author_models:
+        assert author_model.name in ["test_author 1", "test_author 2"]
+    assert len(book_author_models) == 2
+    for book_author_model in book_author_models:
+        assert book_author_model.book_id == book_model.id
+        assert book_author_model.author_id in [author_model.id for author_model in author_models]
+    # Check cover image file
+    assert os.path.exists(f"app/static/images/{test_isbn}.jpg")
+
+    # Clean up
+    os.remove(f"app/static/images/{test_isbn}.jpg")
