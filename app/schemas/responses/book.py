b4c0bb766eb93ce7dd988da6eca1fa9bbd2db0c2
oq-Yuki-po <o.yukinko0715@gmail.com>
2024-01-07 04:12:54 +0900

    Update cover_image_base64 field in GoogleBooksApiSaveOut schema

--- app/schemas/responses/book.py
+++ app/schemas/responses/book.py
@@ -7,7 +7,7 @@ class GoogleBooksApiSaveOut(BaseModel):
     title: str = Field(title='title', min_length=1, max_length=255)
     authors: list = Field(title='authors', min_length=1, max_length=255)
     published_at: str = Field(title='published_at', min_length=1, max_length=255)
-    cover_image_base64: str = Field(title='cover_image_base64', min_length=1)
+    cover_image_base64: str = Field(title='cover_image_base64')
 
     model_config = ConfigDict(
         json_schema_extra={
@@ -16,7 +16,7 @@ class GoogleBooksApiSaveOut(BaseModel):
                 'title': 'sample title',
                 'authors': ['sample author'],
                 'published_at': '2021-01-01',
-                'cover_image_base64': 'sample base64 string'
+                'cover_image_base64': 'sample base64'
             }
         }
     )
