70532a52c1bbd6eb6f0471f75d46561f45893438
oq-Yuki-po <o.yukinko0715@gmail.com>
2025-06-29 13:13:12 +0900

    役割と権限の定義を改善し、ユーザーと書籍のアクセス権を明示化

--- app/core/security.py
+++ app/core/security.py
@@ -1,7 +1,21 @@
+class AppRoles:
+    """
+    Class to define roles in the application.
+    Each role can be used to control access to different parts of the application.
+    """
+    ADMIN = "admin"
+    USER = "user"
+    GUEST = "guest"
+
 class AppRoutePermissions:
+    """
+    Class to define permissions for different routes in the application.
+    Each route can have multiple roles that are allowed to access it.
+    """
     class Users:
 
-        GET_ME = ["admin", "user"]
+        GET_ME = [AppRoles.USER, AppRoles.ADMIN]
 
     class Books:
-        POST_GOOGLE_BOOKS = ["admin"]
+        POST_GOOGLE_BOOKS = [AppRoles.ADMIN]
+
